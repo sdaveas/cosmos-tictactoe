39a82acb91f40d3ad2e41a8e079159c9720d5782
Stelios Daveas <sdaveas@gmail.com>
2021-01-04 00:04:01 +0200

    Create new file for accept game tests

---
+++ tests/functional/test_accept_game.py
@@ -0,0 +1,34 @@
+"""
+Functional test for game accept
+"""
+
+from common import USER1, USER2, count_games, create_game, accept_game, get_status
+
+
+def test_accept_game():
+    """
+    Test accept
+    """
+
+    # create a game
+    res = create_game(USER1, USER2)
+    assert res["raw_log"] != "panic"
+
+    # get game id
+    game_id = count_games() - 1
+
+    # accept a game as provilages; note that ids start from 0
+    res = accept_game(USER2, game_id)
+    assert res["raw_log"] != "panic"
+    assert get_status(game_id) == "RUNNING", "Invalid game state"
+
+    # accept the game again; this should fail
+    res = accept_game(USER2, game_id)
+    assert res["raw_log"] == "panic"
+
+    # accept the game as not privilaged user; this should fail
+    res = accept_game(USER1, game_id)
+    assert res["raw_log"] == "panic"
+
+
+test_accept_game()
